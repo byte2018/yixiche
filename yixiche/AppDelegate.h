6abcb7e36d907bb8f4e864807ab5d610f95adcb3
王震 <zhen.tengjun@qq.com>
2015-03-21 14:59:14 +0800

    Initial Commit

---
+++ yixiche/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yixiche
+//
+//  Created by 王震 on 15/3/21.
+//  Copyright (c) 2015年 nibi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yixiche/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yixiche
+//
+//  Created by 王震 on 15/3/21.
+//  Copyright (c) 2015年 nibi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
